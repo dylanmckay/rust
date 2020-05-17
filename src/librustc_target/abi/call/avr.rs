508e9e19dea4be0edcec4718f1ebffc0b8dec31d
Dylan McKay <me@dylanmckay.io>
2020-05-17 16:36:23 +1200

    [wip] implement AVR calling convention properly

--- src/librustc_target/abi/call/avr.rs
+++ src/librustc_target/abi/call/avr.rs
@@ -1,7 +1,19 @@
-#![allow(non_upper_case_globals)]
+// Reference: avr-gcc Wiki
+// https://gcc.gnu.org/wiki/avr-gcc
 
 use crate::abi::call::{ArgAbi, FnAbi};
 
+// avr-gcc Wiki § Calling Convention
+//
+// "Return values with a size of 1 byte up to and including a size of 8
+// bytes will be returned in registers. Return values whose size is outside
+// that range will be returned in memory."
+//
+//
+// "If a return value cannot be returned in registers, the caller will
+// allocate stack space and pass the address as implicit first pointer
+// argument to the callee. The callee will put the return value into
+// the space provided by the caller."
 fn classify_ret_ty<Ty>(ret: &mut ArgAbi<'_, Ty>) {
     if ret.layout.is_aggregate() {
         ret.make_indirect();
@@ -10,6 +22,12 @@ fn classify_ret_ty<Ty>(ret: &mut ArgAbi<'_, Ty>) {
     }
 }
 
+// avr-gcc Wiki § Calling Convention
+//
+// "An argument is passed either completely in registers or completely in memory."
+//
+// "Arguments of varargs functions are passed on the stack. This applies
+// even to the named arguments."
 fn classify_arg_ty<Ty>(arg: &mut ArgAbi<'_, Ty>) {
     if arg.layout.is_aggregate() {
         arg.make_indirect();
